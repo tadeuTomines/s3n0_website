a45e3782634f377ab4b153a0a3e552dc9d8cb1c3
Tadeu Filho <tadeu.ttff@gmail.com>
2023-06-30 20:29:22 -0300

    Cuidando da responsividade dos releases

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-
 window.onload = () => {
-
-
+    const releases = document.getElementById("releases");
+    const lancamentos = document.getElementById('lancamentos');
+    releases.addEventListener("click", () => {
+        window.location
+        alert('Bora!');
+    });
 }
